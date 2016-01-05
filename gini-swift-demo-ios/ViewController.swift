f1c85a7c4c49d15b0b9a81dfe4aa67195cd30777
Peter Pult <p.pult@gini.net>
2016-01-05 12:58:24 +0100

    Initial Commit

---
+++ gini-swift-demo-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gini-swift-demo-ios
+//
+//  Created by Gini on 05/01/16.
+//  Copyright © 2016 Gini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
