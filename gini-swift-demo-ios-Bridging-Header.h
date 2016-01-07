6584a1e2297db424540d51afdb503fcd086a9dca
Peter Pult <p.pult@gini.net>
2016-01-07 18:41:24 +0100

    Add GiniVisionSDK and GiniSDK to showcase functionality
    Use the GiniVisionSDK to load a photo modul,
    which automatically snaps a picture when an invoice is detected.
    Analyze the document with the GiniSDK and present payment relevant data
    in a table view.
    
    Add settings to configure persistancy of documents on the device.

---
+++ gini-swift-demo-ios-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  gini-swift-demo-ios-Bridging-Header.h
+//  gini-swift-demo-ios
+//
+//  Created by Gini on 07/01/16.
+//  Copyright © 2016 Gini. All rights reserved.
+//
+
+#ifndef gini_swift_demo_ios_Bridging_Header_h
+#define gini_swift_demo_ios_Bridging_Header_h
+
+#import <GiniVision/GiniVision.h>
+
+#endif /* gini_swift_demo_ios_Bridging_Header_h */
